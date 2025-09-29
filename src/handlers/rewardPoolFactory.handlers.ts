16505fa91165f6a69f65273adb23e38bd2e527a5
prevostc <998369+prevostc@users.noreply.github.com>
2025-09-29 21:06:22 +0200

    Add missing factories and contracts

--- src/handlers/rewardPoolFactory.handlers.ts
+++ src/handlers/rewardPoolFactory.handlers.ts
@@ -9,3 +9,12 @@ RewardPoolFactory.RewardPoolCreated.contractRegister(async ({ event, context }) 
 
     context.log.info('RewardPoolCreated', { contractAddress });
 });
+
+RewardPoolFactory.RewardPoolCreatedWithName.contractRegister(async ({ event, context }) => {
+    const contractAddress = event.params.proxy.toString().toLowerCase();
+    if (isVaultBlacklisted(event.chainId, contractAddress)) return;
+
+    context.addRewardPool(contractAddress);
+
+    context.log.info('RewardPoolCreatedWithName', { contractAddress });
+});
